25f315779b8bc7ec6653a9ac92da18fe82493092
Jojo <jghattas7@outlook.com>
2023-10-07 18:18:19 -0400

    Updated Boss Classes for story

--- BossClasses.py
+++ BossClasses.py
@@ -9,11 +9,31 @@ class CurroptedKing():
     def __init__(self):
         self.name = "The Curropted King"
         self.health = 19
-        self.attack = 10
+        self.attack = 11
         self.speed = 3
+
+class ElfKing():
+    def __init__ (self):
+        self.name = "Elf King Algiar"
+        self.health = 17
+        self.attack = 12
+        self.speed = 6
+
+class ElfGeneral():
+    def __init__(self):
+        self.name = "Elf General Sven"
+        self.health = 15
+        self.attack = 10
+        self.speed = 6
+
+
 
 
 b1 = Dragon()
 
 b2 = CurroptedKing()
+
+b3 = ElfKing()
+
+b4 = ElfGeneral()
 
