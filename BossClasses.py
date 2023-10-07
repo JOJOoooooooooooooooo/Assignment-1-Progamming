83d1f051ab98f9664a916ec6c3d73a063be5f985
Jojo <jghattas7@outlook.com>
2023-10-07 12:30:22 -0400

    Added Boss Classes

---
+++ BossClasses.py
@@ -0,0 +1,19 @@
+class Dragon():
+    def __init__(self):
+        self.name = "Dragon"
+        self.health = 35
+        self.attack = 14
+        self.speed = 5
+
+class CurroptedKing():
+    def __init__(self):
+        self.name = "The Curropted King"
+        self.health = 19
+        self.attack = 10
+        self.speed = 3
+
+
+b1 = Dragon()
+
+b2 = CurroptedKing()
+
