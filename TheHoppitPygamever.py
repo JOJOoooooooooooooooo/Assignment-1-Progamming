58e852f65e5494610b9c513ed119cda0934834c2
Jojo <jghattas7@outlook.com>
2023-11-26 18:46:44 -0500

    fixed movement and animations, needs adjustments

--- TheHoppitPygamever.py
+++ TheHoppitPygamever.py
@@ -1,5 +1,7 @@
 import pygame
 import sprite_sheet_module
+#import imagio #look at this for gif making
+
 
 #to initialize pygame
 pygame.init()
@@ -9,6 +11,7 @@ pygame.font.init()
 #create game window
 WIDTH, HEIGHT = 768, 384
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+#set Game Window Title
 pygame.display.set_caption("ParallaxHoppitTest")
 
 #for framerate
@@ -27,28 +30,40 @@ for i in range(1, 3):
     bg_images.extend([bg_image1, bg_image2, bg_image3])
 bg_width = bg_images[0].get_width()
 
+
 #The images are now loaded but we need to draw them
 def draw_bg():
-    for x in range(5):
+    for x in range(7):
         speed = 0.5
         for i in bg_images:
-            offset = (x * bg_width) - scroll * speed
+            offset = (x * bg_width) - scroll * speed #scroll and speed maybe affecting player movement
             screen.blit(i, (offset, 0))
             speed += 0.2
+            
 
+#if abs(scroll) > bg_width:  for endless scrolling
+    #scroll= 0
 #Sprite Sheet for player
-sprite_sheet_image = pygame.image.load('gothic-hero-run.png').convert_alpha()
+sprite_sheet_image = pygame.image.load('spritesheet.png').convert_alpha()
 sprite_sheet = sprite_sheet_module.SpriteSheet(sprite_sheet_image)
 
 #create animation list
 animation_list = []
-animation_steps = 12
+animation_steps = [4, 4, 12]
+action = 0
 last_update = pygame.time.get_ticks() # taking the time from when we execute the code
 animation_cooldown = 100 # "100" is in miliseconds
 frame = 0
+step_counter = 0
 
-for x in range(animation_steps):
-    animation_list.append(sprite_sheet.get_image(x, 65, 46, 1.5, "black"))#replace the first number with x because in this loop x will change through the different sprites
+for animation in animation_steps:
+    temp_image_list = []
+    for _ in range(animation):
+        temp_image_list.append(sprite_sheet.get_image(step_counter, 64, 100, 1.5, "black")) # Step counter replaces x in order to get the specific animation to go to the next frame #replace the first number with x because in this loop x will change through the different sprites
+        step_counter += 1
+    animation_list.append(temp_image_list)
+
+
 
 #frame_0 = sprite_sheet.get_image(0, 65, 46, 1.5, "black") # the order in the brackets are "sheet, frame, width, height, scale, colour"
 #frame_1 = sprite_sheet.get_image(1, 65, 46, 1.5, "black") # we dont need to supply the sheet because its already there as an instance
@@ -60,9 +75,16 @@ def draw(player):
     pygame.draw.rect(screen, "red", player)
     
 #Player width and player height and movement
-PLAYER_WIDTH = 65
-PLAYER_HEIGHT = 46
-PLAYER_VELOCITY = 3.5
+PLAYER_WIDTH = 45
+PLAYER_HEIGHT = 67
+PLAYER_VELOCITY_X = 2
+PLAYER_VELOCITY_Y = 15
+jump = False
+
+#creating and spawning player #IMPORTANT!!! KEEPING THIS CODE INSIDE THE GAME LOOP BREAKS PLAYER MOVEMENT, KEEP IT OUTSIDE
+player = pygame.Rect(5 - scroll, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT) #to get the player to spawn at the very edge on the left put x "200" to "0"
+#in brackets it goes, x, y, width height. We did y = height of game minus player height to get the player to spawn at the bottom of the screen.
+#this is always the format wheber you use a rectangle in pygame "pygame.Rect()"
 
 #creating background music
 pygame.mixer.init() # the order for inside the brackets would go ( 44100, -16, 2, 2048) Frequency, size, channels, buffer
@@ -79,11 +101,6 @@ while run:
     #Draw World
     draw_bg()
 
-    #creating and spawning player
-    player = pygame.Rect(200 - scroll, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT) #to get the player to spawn at the very edge on the left put x "200" to "0"
-    #in brackets it goes, x, y, width height. We did y = height of game minus player height to get the player to spawn at the bottom of the screen.
-    #this is always the format wheber you use a rectangle in pygame "pygame.Rect()"
-
     #event handlers
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -95,24 +112,51 @@ while run:
     if current_time - last_update >= animation_cooldown:
         frame += 1
         last_update = current_time
-        if frame >= len(animation_list):
+        if frame >= len(animation_list[action]):
             frame = 0
+        
 
     #show frame images
-    screen.blit(animation_list[frame], (300, 320))
+    screen.blit(animation_list[action][frame], (player.x-30, player.y -30)) #Draw the sprite animation at the players position
     
     #get key presses and speed of scrolling and limitations of scroll
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT] and scroll > 0:
-        scroll -= 3.5
+        player.x -= PLAYER_VELOCITY_X
+        scroll -= PLAYER_VELOCITY_X
+        action = 1
         
-    if key[pygame.K_RIGHT] and scroll < 1900:
-        scroll += 3.5
+    
+
+    if key[pygame.K_RIGHT] and scroll < 1900 and WIDTH - PLAYER_WIDTH:
+        player.x += PLAYER_VELOCITY_X
+        scroll += PLAYER_VELOCITY_X
+        action =2
+        frame = -1 #This makes it so that everytime it goes above the frames that we have, it will restart problem is when holding button it looks goofy   
+        
+    else:
+        action = 0
+    
+        
+    
+    #Jump Button
+    if jump is False and key[pygame.K_SPACE]:
+        jump = True
+
+    if jump is True:
+        player.y -= PLAYER_VELOCITY_Y
+        PLAYER_VELOCITY_Y -= 1
+        action = 1
+        frame=0
+        if PLAYER_VELOCITY_Y < -15:  
+            jump = False
+            PLAYER_VELOCITY_Y = 15
+            
+
+    #if key[pygame.K_SPACE]:
+      #  player.y -= 10
     #Character movement key presses
-    if key[pygame.K_LEFT] and player.x > 0:
-        player.x -= PLAYER_VELOCITY
-    if key[pygame.K_RIGHT] and player.x < WIDTH - PLAYER_WIDTH: #Player movement does not currently work
-        player.x += PLAYER_VELOCITY
+    
     pygame.display.update()
 
 pygame.quit()
